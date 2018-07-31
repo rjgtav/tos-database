1b1ddd29335435eb9b3d372c328311b3c0c55832
rjgtav <rjgtav@hotmail.com>
2018-07-31 08:59:46 +0100

    IPFParser: updated gitignore

---
+++ IPFParser/src/ipf_parser/utils/fileutil.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+
+# Thanks to https://stackoverflow.com/a/7420617
+def move_tree(source, destination):
+    for src_dir, dirs, files in os.walk(source):
+        dst_dir = src_dir.replace(source, destination, 1)
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                os.remove(dst_file)
+            shutil.move(src_file, dst_dir)
