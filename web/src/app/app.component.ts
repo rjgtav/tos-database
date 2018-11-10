55df1f0a5de37d2a2c1b72ffd46887e4855e32bd
rjgtav <rjgtav@hotmail.com>
2018-11-10 00:19:47 +0000

    Web: cache & image assets optimization

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NgbTooltipConfig} from "@ng-bootstrap/ng-bootstrap";
 import {NavigationEnd, Router} from "@angular/router";
+import {SEOService} from "./shared/service/seo.service";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent {
 
   private page: string;
 
-  constructor(private ngbTooltipConfig: NgbTooltipConfig, private router: Router) {
+  constructor(private ngbTooltipConfig: NgbTooltipConfig, private router: Router, private seo: SEOService) {
     this.ngbTooltipConfig.disableTooltip = !!('ontouchstart' in window || navigator.msMaxTouchPoints);
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
