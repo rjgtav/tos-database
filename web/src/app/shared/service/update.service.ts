70e86c83ebcf76968f2a1b686da6e7b740f89057
Ricardo Tavares <rjgtav@hotmail.com>
2019-01-03 22:59:35 +0000

    [kTOS] Updated database as of 2019-01-03

--- web/src/app/shared/service/update.service.ts
+++ web/src/app/shared/service/update.service.ts
@@ -7,7 +7,7 @@ const VERSION: { [key in TOSRegion]: string } = {
   'iTOS': '234446001001', /* iTOS-needle */
   'jTOS': '233760001001', /* jTOS-needle */
   'kTEST': '234350001001', /* kTEST-needle */
-  'kTOS': '234620_001001.ipf', /* kTOS-needle */
+  'kTOS': '234620001001', /* kTOS-needle */
 };
 
 @Injectable({
