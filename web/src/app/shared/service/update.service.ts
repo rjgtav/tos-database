9313ba89bba324a73e9b64016ca43115ac53e69a
Ricardo Tavares <rjgtav@hotmail.com>
2019-01-03 23:12:27 +0000

    [jTOS] Updated database as of 2019-01-03

--- web/src/app/shared/service/update.service.ts
+++ web/src/app/shared/service/update.service.ts
@@ -5,7 +5,7 @@ const KEY_VERSION = 'version';
 const VERSION_HOTFIX = 0;
 const VERSION: { [key in TOSRegion]: string } = {
   'iTOS': '234446001001_hotfix_1', /* iTOS-needle */
-  'jTOS': '233760001001', /* jTOS-needle */
+  'jTOS': '233760001001_hotfix_1', /* jTOS-needle */
   'kTEST': '234350001001_hotfix_1', /* kTEST-needle */
   'kTOS': '234620001001', /* kTOS-needle */
 };
