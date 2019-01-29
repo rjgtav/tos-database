a6c6a55b6585ee6b28086a93474edbf824a443c6
Ricardo Tavares <rjgtav@hotmail.com>
2019-01-29 07:22:04 +0000

    Updated iTOS as of 2019-01-29

--- web/src/app/shared/service/update.service.ts
+++ web/src/app/shared/service/update.service.ts
@@ -4,7 +4,7 @@ import {TOSRegion, TOSRegionService} from "../domain/tos-region";
 const KEY_VERSION = 'version';
 const VERSION_HOTFIX = 1;
 const VERSION: { [key in TOSRegion]: string } = {
-  'iTOS': 'patch_236993_release_236993', /* iTOS-needle */
+  'iTOS': 'patch_237018_release_236993', /* iTOS-needle */
   'jTOS': 'patch_236290_release_236290', /* jTOS-needle */
   'kTEST': 'patch_236162_release_236140', /* kTEST-needle */
   'kTOS': 'patch_236543_release_236484', /* kTOS-needle */
