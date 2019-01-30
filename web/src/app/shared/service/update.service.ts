0d5ff5af94f7fd7e2c47253325a50f75de9fd99c
Ricardo Tavares <rjgtav@hotmail.com>
2019-01-30 02:22:15 +0000

    Updated jTOS as of 2019-01-30

--- web/src/app/shared/service/update.service.ts
+++ web/src/app/shared/service/update.service.ts
@@ -5,7 +5,7 @@ const KEY_VERSION = 'version';
 const VERSION_HOTFIX = 1;
 const VERSION: { [key in TOSRegion]: string } = {
   'iTOS': 'patch_237018_release_236993', /* iTOS-needle */
-  'jTOS': 'patch_236290_release_236290', /* jTOS-needle */
+  'jTOS': 'patch_237033_release_237033', /* jTOS-needle */
   'kTEST': 'patch_237031_release_237031', /* kTEST-needle */
   'kTOS': 'patch_236543_release_236484', /* kTOS-needle */
 };
