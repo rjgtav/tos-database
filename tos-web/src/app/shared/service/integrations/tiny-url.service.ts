dc809307fd17604b2bcf3b62561b180f764172e7
rjgtav <rjgtav@hotmail.com>
2019-03-07 22:20:17 +0000

    Added tos-web-rest, to provide a simple node API server
    - We're now proxying tinyurl links ourselves. No need anymore for a 3rd party CORS server

--- tos-web/src/app/shared/service/integrations/tiny-url.service.ts
+++ tos-web/src/app/shared/service/integrations/tiny-url.service.ts
@@ -1,9 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
-
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root'
@@ -13,24 +11,14 @@ export class TinyUrlService {
   constructor(private http: HttpClient) { }
 
   create(url: string): Observable<string> {
-    let params = new HttpParams().set('url', url);
     return this.http
-      .get(CORS_PROXY + 'https://tinyurl.com/api-create.php', { params, responseType: 'text' })
+      .post('/api/tinyurl/create', { url }, { responseType: 'text' })
       .pipe(map(value => (value + '').split('http://tinyurl.com/')[1]));
   }
 
   parse(id: string): Observable<any> {
     return this.http
-      .get(CORS_PROXY + 'https://preview.tinyurl.com/' + id, { responseType: 'text' })
-      .pipe(map(value => {
-        let html = document.createElement( 'html' );
-            html.innerHTML = value;
-
-        let url = html.getElementsByTagName('blockquote')[0].innerText;
-            url = url.split(/\r?\n/).join('');
-
-        return url;
-      }));
+      .get('/api/tinyurl/get', { params: { id }, responseType: 'text' });
   }
 
 }
