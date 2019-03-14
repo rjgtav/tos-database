10495af32e678b0eae2c1a541dab7d44c7766ca7
Ricardo Tavares <rjgtav@hotmail.com>
2019-03-14 00:23:16 +0000

    Updated kTOS as of 2019-03-14

--- web/src/app/shared/service/update.service.ts
+++ web/src/app/shared/service/update.service.ts
@@ -7,7 +7,7 @@ const VERSION: { [key in TOSRegion]: string } = {
   'iTOS': 'patch_241328_release_241328', /* iTOS-needle */
   'jTOS': 'patch_241309_release_241309', /* jTOS-needle */
   'kTEST': 'patch_240560_release_240582', /* kTEST-needle */
-  'kTOS': 'patch_241126_release_240766', /* kTOS-needle */
+  'kTOS': 'patch_241507_release_241507', /* kTOS-needle */
   'twTOS': 'patch_241250_release_241250', /* twTOS-needle */
 };
 
