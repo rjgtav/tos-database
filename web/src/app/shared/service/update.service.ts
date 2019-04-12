62cffc4aef3e33cb7f425f5ed56861fa8eeb42e9
Ricardo Tavares <rjgtav@hotmail.com>
2019-04-12 02:23:49 +0000

    Updated iTOS as of 2019-04-12

--- web/src/app/shared/service/update.service.ts
+++ web/src/app/shared/service/update.service.ts
@@ -4,7 +4,7 @@ import {TOSRegion, TOSRegionService} from "../domain/tos-region";
 const KEY_VERSION = 'version';
 const VERSION_HOTFIX = 2;
 const VERSION: { [key in TOSRegion]: string } = {
-  'iTOS': 'patch_244684_release_244776', /* iTOS-needle */
+  'iTOS': 'patch_244684_release_245027', /* iTOS-needle */
   'jTOS': 'patch_244710_release_244710', /* jTOS-needle */
   'kTEST': 'patch_240560_release_240582', /* kTEST-needle */
   'kTOS': 'patch_241507_release_241507', /* kTOS-needle */
