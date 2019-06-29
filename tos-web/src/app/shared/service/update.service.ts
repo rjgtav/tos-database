21fe28abaaef27230509e701fff4ae787b36341c
rjgtav <rjgtav@hotmail.com>
2019-06-29 23:22:02 +0100

    tos-web| Fixed missing translations in twTOS Simulator. Thanks 蘋果🍎

--- tos-web/src/app/shared/service/update.service.ts
+++ tos-web/src/app/shared/service/update.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {TOSRegion, TOSRegionService, VERSIONS} from "../domain/tos-region";
 
 const KEY_VERSION = 'version';
-const VERSION_HOTFIX = 4;
+const VERSION_HOTFIX = 5;
 
 @Injectable({
   providedIn: 'root'
